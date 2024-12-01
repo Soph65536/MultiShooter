acdb629fb29078ba2bf006a70885c417bb9ffd18
Soph <sophearle16@gmail.com>
2024-12-01 13:50:03 +0000

    made unreal project

---
+++ MultiPlayerShooter/Source/MultiPlayerShooter/MultiPlayerShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiPlayerShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSHOOTER_API AMultiPlayerShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
